2a39b0c3fc874adce67c6084d453c0e1ac1f5ec8
chrisharley81 <chrisharley81@gmail.com>
2019-12-24 20:30:05 -0500

    switch functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,12 @@ const checkBox = document.querySelector('input');
 const html = document.querySelector('html');
 
 function update(bgColor, txtColor) {
-  document.body.backgroundColor = bgColor;
-  document.body.color = txtColor;
+  html.style.backgroundColor = bgColor;
+  html.style.color = txtColor;
+}
+
+checkBox.addEventListener('change', changeColor);
+
+function changeColor(e) {
+  (e.target.checked) ? update('#333', '#fff'): update('#fff', '#333');
 }
