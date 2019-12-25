e59abe222652de65cb6c47e85499c07b1b0663e0
chrisharley81 <chrisharley81@gmail.com>
2019-12-24 20:22:52 -0500

    initial html

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+const checkBox = document.querySelector('input');
+const html = document.querySelector('html');
+
+function update(bgColor, txtColor) {
+  document.body.backgroundColor = bgColor;
+  document.body.color = txtColor;
+}
